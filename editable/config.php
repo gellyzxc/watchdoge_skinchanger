cc14a7809fdea825c9ee26ce48c92ae861d86328
Юрочка Сафин <50029191+gellyzxc@users.noreply.github.com>
2022-01-26 17:48:57 +0500

    Update config.php

--- editable/config.php
+++ editable/config.php
@@ -24,8 +24,6 @@ define('DETECT_FILE', '/detects.txt'); // лежит в папке detections
 
 define('BUY_SUBTITLE', 'Оформить подписку на ' . SHORTNAME . '.');
 
-define('BUY_WRAP', '<iframe src="https://yoomoney.ru/quickpay/shop-widget?writer=seller&targets=WATCHDOGE%20SKINCHANGER%20%7C%20%D0%9E%D0%9F%D0%9B%D0%90%D0%A2%D0%90%20150%20%D0%A0%D0%A3%D0%91%D0%9B%D0%95%D0%99&targets-hint=&default-sum=150&button-text=12&payment-type-choice=on&mobile-payment-type-choice=on&mail=on&hint=&successURL=https%3A%2F%2Fwatchdoge.ml%2Fbuy%2Fgood%2F%3Fbuyer_id%3D4KymZgsV%26drain%3Dfalse&quickpay=shop&account=4100117487265695&" width="423" height="250" frameborder="0" allowtransparency="true" scrolling="no"></iframe>');
-
 define('BUY_SUMMA', '150');
 
 define('NAVBAR_MAIN', $url);
